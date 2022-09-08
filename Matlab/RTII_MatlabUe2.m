4a8b5ea814234c035dd2a7109bd2004ce760f7ba
Valentin Nimmervoll <valnim46@gmail.com>
2022-09-08 17:21:31 +0200

    start of work on ue2 polvorgabe

--- Matlab/RTII_MatlabUe2.m
+++ Matlab/RTII_MatlabUe2.m
@@ -29,3 +29,35 @@ Rs = tf(numRs,denRs); % Reihenfolge beachten! (Zählervektor,Nennervektor)
 A = [1,2;3,4]; b = [1;2];
 x1 = A\b;
 x2 = A^-1*b;
+
+
+%% Aufgabe 1
+clc
+clear all
+s = tf('s');
+
+%Testbeispiel der Funktionen
+%Ts = (s+2)/((s+1)*(s+3));
+Ts = (s-1)/((s+1)^2*(s+3));
+%Ts = 1;
+Ps = ((s+2)*(s-1))/(s*(s^2+2*s+1));
+
+%Überprüfung
+myisimp(Ts,Ps);
+
+%% Aufgabe 2
+nu = [1, 2, 3];
+mu = [3,4];
+rho= 1;
+Res = resultante(nu,mu,rho);
+
+%% Aufgabe 3
+clc
+clear all
+s = tf('s');
+Ps = (s-2) / (s*(s-1));
+
+nut = [1, 3, 3, 1];
+
+[R, T] = polvorgabe(Ps, nut);
+
---
+++ Matlab/polvorgabe.m
@@ -0,0 +1,10 @@
+function [R,T] = polvorgabe(P,nut)
+%POLVORGABE Summary of this function goes here
+%   Detailed explanation goes here
+
+%res = resultante()
+
+R = P;
+T = nut;
+end
+
