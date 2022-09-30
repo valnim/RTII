5ccdd674a1947fdc11ff623d076aa08f68cedf1d
Valentin Nimmervoll <valnim46@gmail.com>
2022-09-30 11:29:57 +0200

    sync

--- Matlab/myisimpl_extended.m
+++ Matlab/myisimpl_extended.m
@@ -10,25 +10,48 @@ if myisstable(T) == 1 % T(s) is Bibo Stabil
     nu = zero(P);
     nut = zero(T);
     
+    T_neu = T;
     for i = 1:length(nu)
         if (real(nu(i)) > 0) && ~ismember(nu(i), nut)
             % T(s) modifizieren
-            T = T * (s - real(nu(i)));
+            T_neu = T_neu * (s - real(nu(i)));
             modify_flag = 1;
         end
     end
 
     % Polüberschuss prüfen
-    if (length(pole(T)) - length(zero(T))) < (length(pole(P)) - length(zero(P)))
+    if (length(pole(T_neu)) - length(zero(T_neu))) < (length(pole(P)) - length(zero(P)))
         %T(s) modifizieren
-        k = -((length(pole(T)) - length(zero(T))) - (length(pole(P)) - length(zero(P))));
-        T = T / (s+2)^k;
+        k = -((length(pole(T_neu)) - length(zero(T_neu))) - (length(pole(P)) - length(zero(P))));
+        T_neu = T_neu / (s+2)^k;
         modify_flag = 1;
     end
     isimp_ext__flag = 1;
-    Tresult = T;
+    
+    figure(1)
+    clf
+    step(T, 10);
+    grid on;
+    hold on;
+
+    if modify_flag == 1
+        Tresult = T_neu;
+        step(T_neu, 10);
+        legend('T', 'T_neu')
+    else
+        Tresult = T;
+        legend('T')
+    end
+    
+
+
 else      % T(s) is nicht Bibo Stabil
     isimp_ext__flag = 0;
     Tresult = [];
+    
+    figure(1)
+    clf
+    step(T, 10);
+    grid on;
 end
 end
